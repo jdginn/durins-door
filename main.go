af8a0b4d7db0cc1164173eec060c7a995d44539e
Justin Ginn <Justindavid.ginn@gmail.com>
2021-12-03 00:16:35 -0600

    Types are correct for data_location parsing; algorithm is wrong

--- main.go
+++ main.go
@@ -10,7 +10,21 @@ import (
 	"os"
 )
 
-func parse_location(location []uint8) {
+func parse_location(location []uint8) int64 {
+  // Ignore the first entry in the slice
+  // --> This somehow communicates a format?
+  // Build the last slice from right to left
+  location = location[1:]
+  for i := 0; i < len(location)/2; i++ {
+    j := len(location) - i - 1
+    location[i], location[j] = location[j], location[i]
+  }
+  var location_as_int int64
+  location_as_int = 0
+  for i := 0; i < len(location); i++ {
+    location_as_int += int64(location[i]) << 8
+  }
+  return location_as_int
 }
 
 func follow_typedef(entry *dwarf.Entry) {
@@ -31,20 +45,20 @@ func print_die_info(entry *dwarf.Entry) {
       fmt.Printf("  DW_AT_byte_size: %d\n", byte_size)
     }
     if field.Attr == dwarf.AttrLocation{
-      location := field.Val
-      fmt.Printf("  DW_AT_location: %v\n", location)
+      location := field.Val.([]uint8)
+      fmt.Printf("  DW_AT_location: %x\n", parse_location(location))
     }
     if field.Attr == dwarf.AttrDataMemberLoc{
       location := field.Val
-      fmt.Printf("  DW_AT_data_member_location: %v\n", location)
+      fmt.Printf("  DW_AT_data_member_location: %x\n", location)
     }
     if field.Attr == dwarf.AttrCompDir {
       comp_dir := field.Val
-      fmt.Printf("  DW_AT_comp_dir: %v\n", comp_dir)
+      fmt.Printf("  DW_AT_comp_dir: %s\n", comp_dir)
     }
     if field.Attr == dwarf.AttrType{
       type_die := field.Val
-      fmt.Printf("  DW_AT_type_die offset?: %v\n", type_die)
+      fmt.Printf("  DW_AT_type_die ... TODO: offset?: %v\n", type_die)
     }
   }
 }
