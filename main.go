f526b2809e67f6b734dc823289f831bb8340f3ea
Justin Ginn <Justindavid.ginn@gmail.com>
2021-12-02 22:11:47 -0600

    More flexible

--- main.go
+++ main.go
@@ -36,21 +36,21 @@ func main() {
       fmt.Println("Encountered a nil entry")
       break
     }
-		// if entry.Tag == dwarf.TagBaseType  | dwarf.TagVariable {
-		if 1 == 1 {
-			// fmt.Println("Found a struct")
-      fmt.Printf("Found a %s\n", entry.Tag)
-      name := ""
-      var byte_size int64
-			for _, field := range entry.Field {
-        if field.Attr == dwarf.AttrName {
-          name = field.Val.(string)
-        }
-        if field.Attr == dwarf.AttrByteSize {
-          byte_size = field.Val.(int64)
-        }
-			}
-      fmt.Printf("  Name: %s \n  Size: %d \n\n", name, byte_size)
-		}
+    // fmt.Println("Found a struct")
+    fmt.Printf("Found a %s\n", entry.Tag)
+    name := ""
+    var byte_size int64
+    fmt.Printf("  Attributes:\n")
+    for _, field := range entry.Field {
+      fmt.Printf("      %s\n", field.Attr)
+      if field.Attr == dwarf.AttrName {
+        name = field.Val.(string)
+        fmt.Printf("  DW_AT_name: %s\n", name)
+      }
+      if field.Attr == dwarf.AttrByteSize {
+        byte_size = field.Val.(int64)
+        fmt.Printf("  DW_AT_byte_size: %d\n", byte_size)
+      }
+    }
 	}
 }
