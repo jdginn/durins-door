107d0d6dcf0b8d1e521247e575e51ad863f3d307
Justin Ginn <Justindavid.ginn@gmail.com>
2021-12-02 22:40:42 -0600

    Split print function out of main

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-// import "debug/dwarf"
 import (
 	"debug/dwarf"
 	// "debug/elf"
@@ -10,6 +9,26 @@ import (
 	"log"
 	"os"
 )
+
+func print_die_info(entry *dwarf.Entry) {
+  fmt.Printf("Found a %s\n", entry.Tag)
+  fmt.Printf("  Attributes:\n")
+  for _, field := range entry.Field {
+    fmt.Printf("      %s\n", field.Attr)
+    if field.Attr == dwarf.AttrName {
+      name := field.Val.(string)
+      fmt.Printf("  DW_AT_name: %s\n", name)
+    }
+    if field.Attr == dwarf.AttrByteSize {
+      byte_size := field.Val.(int64)
+      fmt.Printf("  DW_AT_byte_size: %d\n", byte_size)
+    }
+    if field.Attr == dwarf.AttrLocation{
+      location := field.Val
+      fmt.Printf("  DW_AT_location: %v\n", location)
+    }
+  }
+}
 
 func main() {
 	filename := os.Args[1]
@@ -36,21 +55,6 @@ func main() {
       fmt.Println("Encountered a nil entry")
       break
     }
-    // fmt.Println("Found a struct")
-    fmt.Printf("Found a %s\n", entry.Tag)
-    name := ""
-    var byte_size int64
-    fmt.Printf("  Attributes:\n")
-    for _, field := range entry.Field {
-      fmt.Printf("      %s\n", field.Attr)
-      if field.Attr == dwarf.AttrName {
-        name = field.Val.(string)
-        fmt.Printf("  DW_AT_name: %s\n", name)
-      }
-      if field.Attr == dwarf.AttrByteSize {
-        byte_size = field.Val.(int64)
-        fmt.Printf("  DW_AT_byte_size: %d\n", byte_size)
-      }
-    }
+    print_die_info(entry)
 	}
 }
