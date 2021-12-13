b4acd54686d4d5c3a7176e680230cb5c82caad4c
Justin Ginn <Justindavid.ginn@gmail.com>
2021-12-13 17:28:43 -0600

    Add first test

---
+++ parser/parser_test.go
@@ -0,0 +1,18 @@
+package parser
+
+import "testing"
+
+func TestGetReader(t *testing.T) {
+}
+
+func TestHasAttr(t *testing.T) {
+}
+
+func TestParseLocation(t *testing.T) {
+}
+
+func TestGetTypeDie(t *testing.T) {
+}
+
+func TestListAllAttributes(t *testing.T) {
+}
