2b68447ac64447ff81774078aa45f6f4ebcc280c
Justin Ginn <Justindavid.ginn@gmail.com>
2021-12-15 17:23:35 -0600

    Initial TypeDefProxy implementation and roadmap

--- parser/proxies.go
+++ parser/proxies.go
@@ -1,28 +1,57 @@
 package parser
 
 import (
-  "debug/dwarf"
-  "fmt"
+	"debug/dwarf"
+	// "fmt"
 )
 
 // Description of
 type TypeEntryProxy struct {
-  Name string
-  Offset int
-  BitSize int
-  // entry *dwarf.Entry
-  // children map[string]TypedefProxy
+	Name    string
+	Offset  int
+	BitSize int
 }
 
 func NewTypeEntryProxy(reader *dwarf.Reader, e *dwarf.Entry) *TypeEntryProxy {
-  fmt.Println(e.AttrField(dwarf.AttrName).Val)
-  typeEntry, _ := GetTypeEntry(reader, e)
-  proxy := &TypeEntryProxy{
-    Name: e.AttrField(dwarf.AttrName).Val.(string),
-    Offset: int(typeEntry.Offset),
-    BitSize: GetBitSize(typeEntry),
-  }
-  return proxy
+	typeEntry, _ := GetTypeEntry(reader, e)
+	proxy := &TypeEntryProxy{
+		Name:    e.AttrField(dwarf.AttrName).Val.(string),
+		Offset:  int(typeEntry.Offset),
+		BitSize: GetBitSize(typeEntry),
+	}
+	return proxy
+}
+
+// Outward-facing representation of a typedef representing what a user
+// may care about without any DWARF implementation information. This proxy
+// represents the key information required to understand the layout of a particular type
+// type and then read or create variables of this type idiomatically from within a target
+// language (most immediately relevant is Go but this should be generic enough to apply
+// to other languages through Go bindings).
+//
+// All relevant DWARF parsing is handled when this proxy is created and no intermediate
+// DWARF data is included here. The proxy should be ready to hand off as-is to a user.
+type TypeDefProxy struct {
+	Name     string
+	BitSize  int
+	Children map[string]TypeEntryProxy
+}
+
+func NewTypeDefProxy(reader *dwarf.Reader, e *dwarf.Entry) *TypeDefProxy {
+	typeEntry, _ := GetTypeEntry(reader, e)
+	proxy := &TypeDefProxy{
+		Name:     e.AttrField(dwarf.AttrName).Val.(string),
+		BitSize:  0,
+		Children: make(map[string]TypeEntryProxy),
+	}
+
+	// TODO: this probably needs an else case where we compute size from walking
+  // the typedef, which we will do anyway.
+	if hasAttr(typeEntry, dwarf.AttrByteSize) || hasAttr(typeEntry, dwarf.AttrBitSize) {
+		proxy.BitSize = GetBitSize(typeEntry)
+	}
+
+	return proxy
 }
 
 // TODO: this should probably be a new thing called TypeDefProxy, representing the full TypeDef
@@ -39,9 +68,9 @@ func NewTypeEntryProxy(reader *dwarf.Reader, e *dwarf.Entry) *TypeEntryProxy {
 // }
 
 type VariableProxy struct {
-  Name string
-  Address int
-  Size int
-  entry *dwarf.Entry
-  children map[string]VariableProxy
+	Name     string
+	Address  int
+	Size     int
+	entry    *dwarf.Entry
+	children map[string]VariableProxy
 }
--- parser/proxies_test.go
+++ parser/proxies_test.go
@@ -10,12 +10,35 @@ func TestNewTypeEntryProxy(t *testing.T) {
   reader, _ := GetReader(testcaseFilename)
   var p *TypeEntryProxy
   var e *dwarf.Entry
+
   e, _ = GetEntry(reader, "Driver")
   p = NewTypeEntryProxy(reader, e)
   assert.Equal(t, p.Name, "Driver")
   assert.Equal(t, p.BitSize, int(12 * 8))
+
   e, _ = GetEntry(reader, "char")
   p = NewTypeEntryProxy(reader, e)
   assert.Equal(t, p.Name, "char")
   assert.Equal(t, p.BitSize, int(8))
+}
+
+func TestNewTypeDefProxy(t *testing.T) {
+  reader, _ := GetReader(testcaseFilename)
+  var p *TypeDefProxy
+  var e *dwarf.Entry
+
+  // Start with a few trivial cases
+  e, _ = GetEntry(reader, "Driver")
+  p = NewTypeDefProxy(reader, e)
+  assert.Equal(t, p.Name, "Driver")
+  assert.Equal(t, p.BitSize, int(12 * 8))
+  assert.Equal(t, p.Children, make(map[string]TypeEntryProxy))
+
+  e, _ = GetEntry(reader, "char")
+  p = NewTypeDefProxy(reader, e)
+  assert.Equal(t, p.Name, "char")
+  assert.Equal(t, p.BitSize, int(8))
+  assert.Equal(t, p.Children, make(map[string]TypeEntryProxy))
+
+  // Move on to non-trivial cases in which Children must actually be populated
 }
