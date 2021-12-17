1ac1d8fe7a19cfc32e8b4cd80fd1d6d703eb6b09
Justin Ginn <Justindavid.ginn@gmail.com>
2021-12-16 21:37:23 -0600

    Re-enable the test we are focusing on

--- parser/proxies_test.go
+++ parser/proxies_test.go
@@ -33,45 +33,45 @@ func TestNewTypeEntryProxy(t *testing.T) {
 	assert.Equal(t, int(8), p.BitSize)
 }
 
-// func TestNewTypeDefProxy(t *testing.T) {
-// 	reader, _ := GetReader(testcaseFilename)
-// 	var p *TypeDefProxy
-// 	var e *dwarf.Entry
+func TestNewTypeDefProxy(t *testing.T) {
+	reader, _ := GetReader(testcaseFilename)
+	var p *TypeDefProxy
+	var e *dwarf.Entry
 
-// 	// Start with a few trivial cases
-// 	e, _ = GetEntry(reader, "char")
-// 	p = NewTypeDefProxy(reader, e)
-// 	assert.Equal(t, "char", p.Name)
-// 	assert.Equal(t, int(8), p.BitSize)
-//   assert.Equal(t, make([]TypeDefProxy, 0), p.Children)
+	// Start with a few trivial cases
+	e, _ = GetEntry(reader, "char")
+	p = NewTypeDefProxy(reader, e)
+	assert.Equal(t, "char", p.Name)
+	assert.Equal(t, int(8), p.BitSize)
+  assert.Equal(t, make([]TypeDefProxy, 0), p.Children)
 
-// 	// Move on to non-trivial cases in which Children must actually be populated
-// 	e, _ = GetEntry(reader, "Driver")
-// 	p = NewTypeDefProxy(reader, e)
-// 	var driverChildren = []TypeDefProxy{
-//     {
-// 			Name:        "initials",
-// 			BitSize:     8,
-// 			DwarfOffset: 0,
-// 			ArrayRanges: []int{2},
-//       Children:    make([]TypeDefProxy, 0),
-// 		},
-//     {
-// 			Name:        "car_number",
-// 			BitSize:     32,
-// 			DwarfOffset: 16,
-// 			ArrayRanges: []int{0},
-//       Children:    make([]TypeDefProxy, 0),
-// 		},
-//     {
-// 			Name:        "has_won_wdc",
-// 			BitSize:     8,
-// 			DwarfOffset: 48,
-// 			ArrayRanges: []int{0},
-//       Children:    make([]TypeDefProxy, 0),
-// 		},
-// 	}
-// 	assert.Equal(t, "Driver", p.Name)
-// 	assert.Equal(t, int(12*8), p.BitSize)
-// 	assert.Equal(t, driverChildren, p.Children)
-// }
+	// Move on to non-trivial cases in which Children must actually be populated
+	e, _ = GetEntry(reader, "Driver")
+	p = NewTypeDefProxy(reader, e)
+	var driverChildren = []TypeDefProxy{
+    {
+			Name:        "initials",
+			BitSize:     8,
+			DwarfOffset: 0,
+			ArrayRanges: []int{2},
+      Children:    make([]TypeDefProxy, 0),
+		},
+    {
+			Name:        "car_number",
+			BitSize:     32,
+			DwarfOffset: 16,
+			ArrayRanges: []int{0},
+      Children:    make([]TypeDefProxy, 0),
+		},
+    {
+			Name:        "has_won_wdc",
+			BitSize:     8,
+			DwarfOffset: 48,
+			ArrayRanges: []int{0},
+      Children:    make([]TypeDefProxy, 0),
+		},
+	}
+	assert.Equal(t, "Driver", p.Name)
+	assert.Equal(t, int(12*8), p.BitSize)
+	assert.Equal(t, driverChildren, p.Children)
+}
