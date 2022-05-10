65fb8cc463b65f67ecb9e151fcecc11a31216e0f
Justin Ginn <Justindavid.ginn@gmail.com>
2022-05-09 22:18:48 -0500

    Add file client tests
    Also: fmt

--- client/file/file.go
+++ client/file/file.go
@@ -1,38 +1,45 @@
 package file
 
 import (
-  "os"
 	"fmt"
+	"os"
 )
 
 type FileClient struct {
-  rw *os.File
+	rw *os.File
 }
 
-func NewFileClient(filename string) (*FileClient, error) {
-  f, err := os.Open(filename)
-  if err != nil {
-    return &FileClient{}, fmt.Errorf("Could not open file %s:\n\n\t%s", filename, err)
-  }
-  fw := &FileClient {
-    rw: f,
-  }
-  return fw, nil
+func NewFileClient(f *os.File) (*FileClient, error) {
+	fw := &FileClient{
+		rw: f,
+	}
+	return fw, nil
+}
+
+func NewFileClientFromName(filename string) (*FileClient, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return &FileClient{}, fmt.Errorf("Could not open file %s:\n\n\t%s", filename, err)
+	}
+	fw := &FileClient{
+		rw: f,
+	}
+	return fw, nil
 }
 
 func (p *FileClient) Read(addr int, size int) ([]byte, error) {
-  val := make([]byte, size)
-  n, err := p.rw.ReadAt(val, int64(addr))
-  if err != nil {
-    return val, err
-  }
-  if n != size {
-    return val, fmt.Errorf("Read the incorrect number of bytes\n Expected: %d bytes; Read %d", size, n)
-  }
-  return val, nil
+	val := make([]byte, size)
+	n, err := p.rw.ReadAt(val, int64(addr))
+	if err != nil {
+		return val, err
+	}
+	if n != size {
+		return val, fmt.Errorf("Read the incorrect number of bytes\n Expected: %d bytes; Read %d", size, n)
+	}
+	return val, nil
 }
 
-func (p *FileClient) Write(addr int, data []byte) error { 
-  _, err := p.rw.WriteAt(data, int64(addr))
-  return err
+func (p *FileClient) Write(addr int, data []byte) error {
+	_, err := p.rw.WriteAt(data, int64(addr))
+	return err
 }
