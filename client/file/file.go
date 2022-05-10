8a0f08af2a1090531c9b0c8a39a032aa053c29f8
Justin Ginn <Justindavid.ginn@gmail.com>
2022-05-09 21:43:29 -0500

    Simplify file client to only read and write files

--- client/file/file.go
+++ client/file/file.go
@@ -3,62 +3,36 @@ package file
 import (
   "os"
 	"fmt"
-  
-  "github.com/jdginn/dwarf-explore"
 )
 
-type ProxyWrapper struct {
-  parser.VariableProxy
+type FileClient struct {
   rw *os.File
 }
 
-// TODO: this needs a lot of work. From the files, construct everything
-// we need for the NewVariableWrapper and then read its contents
-// from binFile.
-//
-// Encapsulate anything having to do with dwarf.
-// Still take filenames (for compatibility) but break out the handling
-// of those so it's not so clunky and they don't need to appear
-// in this package (still make sure they don't have to appear in clients
-// of this package)
-//
-// Keep in mind, the next step here is building a server
-func NewVariableWrapper(dwarfFile parser.DebugFile, binFile *os.File, entryName string) (*ProxyWrapper, error) {
-  dwarfReader, err := parser.GetReader(dwarfFile)
-  if err != nil { return nil, err }
-  entry, err := parser.GetEntry(dwarfReader, entryName)
+func NewFileClient(filename string) (*FileClient, error) {
+  f, err := os.Open(filename)
   if err != nil {
-    return nil, err
+    return &FileClient{}, fmt.Errorf("Could not open file %s:\n\n\t%s", filename, err)
   }
-  // newProxy, err := parser.NewVariableProxy(dwarfReader, entry)
-  p := &ProxyWrapper{
-    rw: binFile,
+  fw := &FileClient {
+    rw: f,
   }
-  // call parser.VariableProxy constructor
-  err = p.Init(dwarfReader, entry)
-  return p, err
+  return fw, nil
 }
 
-func (p *ProxyWrapper) Write() error { 
-  m := p.GetAccessMetadata()
-  value, err := p.Get()
+func (p *FileClient) Read(addr int, size int) ([]byte, error) {
+  val := make([]byte, size)
+  n, err := p.rw.ReadAt(val, int64(addr))
   if err != nil {
-    return err
+    return val, err
   }
-  _, err = p.rw.WriteAt(value, int64(m.Address))
-  return err
+  if n != size {
+    return val, fmt.Errorf("Read the incorrect number of bytes\n Expected: %d bytes; Read %d", size, n)
+  }
+  return val, nil
 }
 
-func (p *ProxyWrapper) Read() error { 
-  m := p.GetAccessMetadata()
-  value := make([]byte, m.Size)
-  n, err := p.rw.ReadAt(value, int64(m.Address))
-  if err != nil {
-    return err
-  }
-  if n != m.Size {
-    return fmt.Errorf("Read the incorrect number of bytes\n Expected: %d bytes; Read %d", m.Size, n)
-  }
-  err = p.Set(value)
+func (p *FileClient) Write(addr int, data []byte) error { 
+  _, err := p.rw.WriteAt(data, int64(addr))
   return err
 }
