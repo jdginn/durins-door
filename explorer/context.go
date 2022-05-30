fa3828e16970c4d474bb06eef83d47c169499c16
Justin Ginn <Justindavid.ginn@gmail.com>
2022-05-30 12:03:29 -0500

    explorer: generalize API across CU, entry, proxy
    - Make context primarily responsible for managing proxies, rather than
      raw entries
    - Generalize API to support exploring either type or variable proxies,
      where possible
        1. Top-level CUs
        2. Entries
        3. Proxies

--- explorer/context.go
+++ explorer/context.go
@@ -2,30 +2,73 @@ package explorer
 
 import (
 	"debug/dwarf"
+	// "sync"
+
+	"github.com/jdginn/durins-door/parser"
 )
 
-type explorerCtx struct {
-	tags   []dwarf.Tag
-	levels []*dwarf.Entry
+type mode int
+
+const (
+	modeCUs mode = iota
+	modeEntry
+	modeProxy
+)
+
+type ctxLevel struct {
+	mode  mode
+	entry *dwarf.Entry
+	proxy parser.Proxy
+}
+
+type stack struct {
+	// mux    sync.Mutex
+	levels []ctxLevel
+}
+
+func NewStack() stack {
+	// s := stack{sync.Mutex{}, make([]ctxLevel, 0, 128)}
+	s := stack{make([]ctxLevel, 0, 128)}
+	s.Push(modeCUs, nil, nil)
+	return s
 }
 
 // Returns the current entry pointed to by this context
-func (c *explorerCtx) CurrEntry() (*dwarf.Entry, bool) {
-	if len(c.levels) <= 0 {
-		return nil, false
+func (c *stack) CurrEntry() *dwarf.Entry {
+	// c.mux.Lock()
+	// defer c.mux.Unlock()
+	return c.levels[len(c.levels)-1].entry
+}
+
+// Returns the current entry pointed to by this context
+func (c *stack) CurrProxy() parser.Proxy {
+	// c.mux.Lock()
+	// defer c.mux.Unlock()
+	return c.levels[len(c.levels)-1].proxy
+}
+
+func (c *stack) CurrMode() mode {
+	if len(c.levels) < 1 {
+		return -1
 	}
-	return c.levels[len(c.levels)-1], true
+	// c.mux.Lock()
+	// defer c.mux.Unlock()
+	return c.levels[len(c.levels)-1].mode
 }
 
-func (c *explorerCtx) Push(e *dwarf.Entry) {
-	c.levels = append(c.levels, e)
+func (c *stack) Push(m mode, e *dwarf.Entry, p parser.Proxy) {
+	// c.mux.Lock()
+	// defer c.mux.Unlock()
+	c.levels = append(c.levels, ctxLevel{m, e, p})
 }
 
-func (c *explorerCtx) Pop() (*dwarf.Entry, bool) {
-	var entry *dwarf.Entry
+func (c *stack) Pop() (ctxLevel, bool) {
+	// c.mux.Lock()
+	// defer c.mux.Unlock()
+	var level ctxLevel
 	if len(c.levels) == 0 {
-		return nil, false
+		return ctxLevel{}, false
 	}
-	entry, c.levels = c.levels[len(c.levels)-1], c.levels[:len(c.levels)-1]
-	return entry, true
+	level, c.levels = c.levels[len(c.levels)-1], c.levels[:len(c.levels)-1]
+	return level, true
 }
--- explorer/explorer.go
+++ explorer/explorer.go
@@ -3,6 +3,7 @@ package explorer
 import (
 	"debug/dwarf"
 	"fmt"
+	// "log"
 	"strings"
 
 	"github.com/jdginn/durins-door/client"
@@ -12,32 +13,21 @@ import (
 
 // Struct that mediates DWARF parsing as well as reading and writing
 type Explorer struct {
-	readerFile string
-	reader     *dwarf.Reader
-	client     client.Client
-	ctx        explorerCtx
+	DwarfFile string
+	reader    *dwarf.Reader
+	client    client.Client
+	ctx       stack
 }
 
 func NewExplorer() *Explorer {
 	return &Explorer{
-		ctx: explorerCtx{},
+		ctx: NewStack(),
 	}
-}
-
-func (e Explorer) CurrEntryName() string {
-	entry, ok := e.ctx.CurrEntry()
-	if !ok {
-		return "Top level"
-	}
-	if !parser.HasAttr(entry, dwarf.AttrName) {
-		return "unnamed entry"
-	}
-	return entry.Val(dwarf.AttrName).(string)
 }
 
 // Creates a reader within this explorer, reading the specified file
 func (e *Explorer) CreateReaderFromFile(fname string) error {
-	e.readerFile = fname
+	e.DwarfFile = fname
 	fh, err := plat.GetReaderFromFile(fname)
 	if err != nil {
 		return err
@@ -50,70 +40,151 @@ func (e *Explorer) CreateReaderFromFile(fname string) error {
 	return nil
 }
 
-// Returns the name of the DWARF file this explorer is reading
-func (e *Explorer) GetReaderFilename() string {
-	return e.readerFile
-}
-
-// Sets this explorer's client
-func (e *Explorer) SetClient(c client.Client) error {
-	e.client = c
-	return nil
-}
-
-func (e *Explorer) GetTypeDefProxy() (*parser.TypeDefProxy, error) {
-	if e.reader == nil {
-		return nil, fmt.Errorf("Cannot get TypeDef proxies without setting a reader. Create a reader using CreateReaderFromFile().")
-	}
-	entry, ok := e.ctx.CurrEntry()
-	if !ok {
-		return nil, fmt.Errorf("Could not find current entry")
-	}
-	p, err := parser.NewTypeDefProxy(e.reader, entry)
+func NewExplorerFromFile(fname string) *Explorer {
+	e := NewExplorer()
+	err := e.CreateReaderFromFile(fname)
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
-	return p, nil
+	return e
 }
 
-func (e *Explorer) ShowAllChildren() ([]string, error) {
-	if e.reader == nil {
-		return nil, fmt.Errorf("Cannot List CUs without setting a reader. Create a reader using CreateReaderFromFile().")
-	}
+func (e *Explorer) listEntryChildren() []string {
 	entries, err := parser.GetChildren(e.reader, func(entry *dwarf.Entry) bool {
 		return (entry.Tag == dwarf.TagVariable || entry.Tag == dwarf.TagCompileUnit)
 	})
 	if err != nil {
-		return []string{}, err
+		return []string{}
 	}
 	ret := make([]string, len(entries), len(entries))
 	for i, e := range entries {
 		ret[i] = e.Val(dwarf.AttrName).(string)
 	}
-	return ret, nil
+	return ret
 }
 
-func (e *Explorer) StepIntoChild(childName string) {
-	entry, _, err := parser.GetEntry(e.reader, childName)
-	if err != nil {
-		panic(err)
+func (e *Explorer) ListChildren() []string {
+	switch e.ctx.CurrMode() {
+	case modeCUs:
+		s, _ := e.ListCUs()
+		return s
+	case modeEntry:
+		return e.listEntryChildren()
+	case modeProxy:
+		return e.ctx.CurrProxy().ListChildren()
+	default:
+		return []string{"No children to display"}
 	}
-	e.ctx.Push(entry)
 }
+
+func (e *Explorer) CurrMode() string {
+	switch e.ctx.CurrMode() {
+	case modeCUs:
+		return "modeCUs"
+	case modeEntry:
+		return "modeEntry"
+	case modeProxy:
+		return "modeProxy"
+	}
+	return "bad mode"
+}
+
+func (e *Explorer) CurrName() string {
+	switch e.ctx.CurrMode() {
+	case modeCUs:
+		return "all CUs"
+	case modeEntry:
+		return e.ctx.CurrEntry().Val(dwarf.AttrName).(string)
+	case modeProxy:
+		return e.ctx.CurrProxy().Name()
+	default:
+		return "bad mode"
+	}
+}
+
+func (e *Explorer) StepIntoChild(childName string) error {
+	switch e.ctx.CurrMode() {
+	case modeCUs:
+		entry, _, err := parser.GetEntry(e.reader, childName)
+		if err != nil {
+			return err
+		}
+		e.ctx.Push(modeEntry, entry, nil)
+		return nil
+	case modeEntry:
+		entry, _, err := parser.GetEntry(e.reader, childName)
+		if err != nil {
+			return err
+		}
+		p, err := e.getProxy(entry)
+		if err != nil {
+			return err
+		}
+		e.ctx.Push(modeProxy, nil, p)
+		return nil
+	case modeProxy:
+		p, err := e.ctx.CurrProxy().GetChild(childName)
+		if err != nil {
+			return err
+		}
+		e.ctx.Push(modeProxy, nil, p)
+		return nil
+	default:
+		return fmt.Errorf("Invalid mode")
+	}
+}
+
+func (e *Explorer) Back() error {
+	e.ctx.Pop()
+	if e.ctx.CurrMode() == modeEntry {
+		e.reader.Seek(e.ctx.CurrEntry().Offset)
+	}
+	return nil
+}
+
+func (e *Explorer) GetType() error {
+	switch e.ctx.CurrProxy().(type) {
+	// If we are already looking at a typeDef, there is nothing to do
+	case parser.TypeDefProxy:
+	case parser.VariableProxy:
+		e.ctx.Push(modeProxy, nil, e.ctx.CurrProxy().(parser.VariableProxy).Type)
+	}
+	return nil
+}
+
+func (e *Explorer) getProxy(entry *dwarf.Entry) (parser.Proxy, error) {
+	switch entry.Tag {
+	case dwarf.TagVariable:
+		return parser.NewVariableProxy(e.reader, entry)
+	case dwarf.TagTypedef:
+		return parser.NewTypeDefProxy(e.reader, entry)
+	default:
+		return nil, fmt.Errorf("Invalid tag %s for entry %s", entry.Tag.String(), parser.FormatEntryInfo(entry))
+	}
+}
+
+// func (e *Explorer) GetTypeDefProxy() (*parser.TypeDefProxy, error) {
+// 	if e.reader == nil {
+// 		return nil, fmt.Errorf("Cannot get TypeDef proxies without setting a reader. Create a reader using CreateReaderFromFile().")
+// 	}
+// 	entry := e.ctx.CurrEntry()
+// 	p, err := parser.NewTypeDefProxy(e.reader, entry)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return p, nil
+// }
 
 func (e *Explorer) Up() bool {
-	entry, ok := e.ctx.Pop()
+	c, ok := e.ctx.Pop()
 	if ok {
-		e.reader.Seek(entry.Offset)
+		e.reader.Seek(c.entry.Offset)
 	}
 	return ok
 }
 
 func (e *Explorer) Info() string {
-	entry, ok := e.ctx.CurrEntry()
-	if !ok {
-		return "Top level: no info to show"
-	}
+	entry := e.ctx.CurrEntry()
 	return parser.FormatEntryInfo(entry)
 
 }
