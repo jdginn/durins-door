d277d902d129979f07a032272b1d1d988fcdc6e4
Justin Ginn <Justindavid.ginn@gmail.com>
2022-07-01 19:01:51 -0500

    WIP: initial REST server

--- servers/rest/server.go
+++ servers/rest/server.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	// "net/http"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 
+	//TODO: is explorer the right tool for the job here?
 	"github.com/jdginn/durins-door/explorer"
 )
-
-type RESTClient struct {
-  e explorer.Explorer
-}
-
-func setDebugFile(c *gin.Context) {}
-
-func getDebugFile(c *gin.Context) {}
 
 func listTypeDefs(c *gin.Context) {}
 
@@ -31,14 +24,32 @@ func getVariableTypeDef(c *gin.Context) {}
 func setVariableValue(c *gin.Context) {}
 
 func main() {
-  server := gin.Default()
-  server.PUT("/debugfile", setDebugFile)
-  server.GET("/debugfile", getDebugFile)
-  server.GET("/typedefs", listTypeDefs)
-  server.GET("/typedefs/:type", getTypeDefByName)
-  server.GET("/typedefs/:type/children", listTypeDefChildren)
-  server.GET("/typedefs/:type/children:child", getTypeDefChild)
-  server.GET("/variables/:variable", getVariable)
-  server.GET("/variables/:variable/typdedef", getVariableTypeDef)
-  server.PUT("/variables/:variable", setVariableValue)
+	server := gin.Default()
+
+	e := explorer.NewExplorer()
+	server.POST("/debugfile", func(c *gin.Context) {
+		type filename struct {
+			Path string `json:"path"`
+		}
+		var message filename
+		if err := c.BindJSON(&message); err != nil {
+			return
+		}
+    if err := e.CreateReaderFromFile(message.Path); err != nil {
+    }
+		c.String(http.StatusAccepted, "")
+	})
+	server.GET("/debugfile", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, e.DwarfFile)
+	})
+	// server.GET("/typedefs", func(c *gin.Context) {
+	//  })
+	server.GET("/typedefs/:type", getTypeDefByName)
+	server.GET("/typedefs/:type/children", listTypeDefChildren)
+	server.GET("/typedefs/:type/children:child", getTypeDefChild)
+	server.GET("/variables/:variable", getVariable)
+	server.GET("/variables/:variable/typdedef", getVariableTypeDef)
+	server.PUT("/variables/:variable", setVariableValue)
+
+	server.Run("localhost:8080")
 }
