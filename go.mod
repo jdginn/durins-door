3dbe0358e204bbe7baf43f40a998769b1b15b0d9
Justin Ginn <Justindavid.ginn@gmail.com>
2022-07-08 14:23:34 -0400

    Add glog; add ping command

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/golang/glog v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -22,6 +23,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/szuecs/gin-glog v1.1.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
--- servers/rest/server.go
+++ servers/rest/server.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"debug/dwarf"
+  "flag"
 	"fmt"
-	"io"
+	// "io"
 	"net/http"
-	"os"
+	// "os"
+  "time"
 
 	"github.com/gin-gonic/gin"
+  "github.com/golang/glog"
+  "github.com/szuecs/gin-glog"
 
 	"github.com/jdginn/durins-door/explorer/plat"
 	"github.com/jdginn/durins-door/parser"
@@ -28,22 +32,33 @@ func getVariableTypeDef(c *gin.Context) {}
 func setVariableValue(c *gin.Context) {}
 
 func main() {
+  flag.Parse() 
+
 	type serverContext struct {
 		debugFile string
 		reader    *dwarf.Reader
 	}
 	s := serverContext{}
 
-	f, err := os.OpenFile("durinsdoor.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	if err != nil {
-		panic(err)
-	}
-	gin.DisableConsoleColor()
-	gin.DefaultWriter = io.MultiWriter(f)
+	// f, err := os.OpenFile("durinsdoor.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// gin.DisableConsoleColor()
+	// gin.DefaultWriter = io.MultiWriter(f)
 
-	server := gin.Default()
+	server := gin.New()
+  server.Use(ginglog.Logger(3 * time.Second))
+  server.Use(gin.Recovery())
 
+  glog.Info("Launching durins-door REST server")
+
+  server.GET("/ping", func(c *gin.Context) {
+    glog.Infof("Responding to ping")
+    c.IndentedJSON(http.StatusOK, "pong")
+  })
 	server.POST("/debugfile", func(c *gin.Context) {
+    glog.Infof("Attempting to set debugfile") 
 		type filename struct {
 			Path string `json:"path"`
 		}
@@ -56,36 +71,40 @@ func main() {
 		fh, err := plat.GetReaderFromFile(s.debugFile)
 		if err != nil {
 			msg := fmt.Sprintf("Error getting opening file %s:\n\t%s", s.debugFile, err)
+      glog.Error(msg)
 			c.IndentedJSON(http.StatusInternalServerError, msg)
 			return
 		}
 		reader, err := parser.GetReader(fh)
 		if err != nil {
 			msg := fmt.Sprintf("Error getting reader from file %s:\n\t%s", s.debugFile, err)
+      glog.Error(msg)
 			c.IndentedJSON(http.StatusInternalServerError, msg)
 			return
 		}
 		s.reader = reader
 
 		c.JSON(http.StatusOK, "looks good to me!")
+    glog.Infof("Successfully set debugfile to %s", message.Path)
     return
 	})
 	server.GET("/debugfile", func(c *gin.Context) {
+    glog.Infof("Returning debugfile path: %s", s.debugFile)
 		c.IndentedJSON(http.StatusOK, s.debugFile)
     return
 	})
 	server.GET("/cus", func(c *gin.Context) {
 		cus, err := parser.GetCUs(s.reader)
 		if err != nil {
-      fmt.Println(err)
-      gin.DefaultWriter.Write([]byte(err.Error()))
-			// c.IndentedJSON(http.StatusInternalServerError, fmt.Sprintf("ListCUs failed with error: %s", err.Error()))
-			c.JSON(http.StatusInternalServerError, "bad list CUs")
+      msg := fmt.Sprintf("/cus failed with error: %s", err.Error())
+      glog.Error(msg) 
+			c.IndentedJSON(http.StatusInternalServerError, msg)
 		}
     ret := make([]string, len(cus))
     for i, c := range cus {
       ret[i] = c.Val(dwarf.AttrName).(string)
     }
+    glog.Infof("Successfully found CUs: %v", ret)
     c.IndentedJSON(http.StatusOK, ret)
 		return
 	})
